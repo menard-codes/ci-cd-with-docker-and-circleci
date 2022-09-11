037770cd0b301a141bccc9791570ccbbaf428598
Menard Maranan <menardmaranan.codes@gmail.com>
2022-09-11 16:17:46 +0800

    added docker files

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 const server = require("./main.js");
 const PORT = process.env.PORT || 3000;
 
-server.listen(PORT, () => console.log(`Listening to port ${PORT}`));
+server.listen(PORT, () => console.log(`Serving the API...`));
